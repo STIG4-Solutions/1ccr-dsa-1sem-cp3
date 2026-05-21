0d8b180d35b0fe7422fe0420be63d57de1898d7b
glaucokelly <glaucokelly@icloud.com>
2026-05-20 21:58:18 -0300

    Adiciona função exibir menu

---
+++ main.c
@@ -0,0 +1,12 @@
+void exibirMenu()
+{
+    printf("\n========================\n");
+    printf(" CAIXA ELETRONICO \n");
+    printf("========================\n");
+    printf("1 - Consultar Saldo\n");
+    printf("2 - Realizar Saque\n");
+    printf("3 - Realizar Deposito\n");
+    printf("0 - Sair\n");
+    printf("========================\n");
+    printf("Escolha uma opcao: ");
+}
